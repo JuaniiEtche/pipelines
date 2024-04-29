bc5f0fbf38a01beda79fe23059a6babdca8ae09a
JuaniiEtche <114359857+JuaniiEtche@users.noreply.github.com>
2024-04-28 23:08:45 -0300

    Update capnee_be.groovy

--- capnee/capnee_be.groovy
+++ capnee/capnee_be.groovy
@@ -1,39 +1,43 @@
-pipeline{
+pipeline {
     agent any
-    environment{
-        // GIT Proyect
+    environment {
+        // Variables de entorno del proyecto GIT
         Branch = "${env.BRANCH}"
         Hostname = "${env.HOSTNAME}"
+        Ambiente = "${env.AMBIENTE}" 
     }
 
-    stages{
+    stages {
         stage("Build Info") {
             steps {
                 script {
-                    BUILD_TRIGGER_BY = currentBuild.getBuildCauses()[0].userId
-                    currentBuild.displayName = "#${env.BUILD_NUMBER} Hostname:${Hostname} Branch:${Branch} By:${BUILD_TRIGGER_BY} "                    
+                    BUILD_TRIGGER_BY = currentBuild.getBuildCauses()[0]?.userId ?: 'unknown'
+                    currentBuild.displayName = "#${env.BUILD_NUMBER} Hostname:${Hostname} Branch:${Branch} By:${BUILD_TRIGGER_BY}"
                 }
             }
-        }    
+        }
 
         stage('Deploy') {
-        steps {
-            script {
-                try {
-                        // Comando SSH para ejecutar el script 
-                        def result = sh(script: "sh /home/ubuntu/capnee/deploy_capnee_be.sh ${Branch}", returnStatus: true)
-
-                        // Verificar el código de salida del comando SSH
+            steps {
+                script {
+                    // Usar diferentes scripts de deployment basados en el valor de Ambiente
+                    if (Ambiente == 'PROD') {
+                        // Ejecutar el script en la VM de producción
+                        def result = sh(script: "ssh root@200.58.106.151 'bash /root/capnee/deploy_capnee_be.sh ${Branch}'", returnStatus: true)
                         if (result != 0) {
-                            error("Error: El comando SSH no se ejecutó correctamente. Código de salida: ${result}")
+                            error("Error: El comando SSH para producción no se ejecutó correctamente. Código de salida: ${result}")
                         }
-                } catch (Exception ex) {
-                    // Capturar cualquier excepción y manejarla
-                    error("Error durante la ejecución del script: ${ex.message}")
+                    } else if (Ambiente == 'UAT') {
+                        // Ejecutar el script en la VM de UAT
+                        def result = sh(script: "sh /home/ubuntu/capnee/deploy_capnee_be.sh ${Branch}", returnStatus: true)
+                        if (result != 0) {
+                            error("Error: El comando SSH para UAT no se ejecutó correctamente. Código de salida: ${result}")
+                        }
+                    } else {
+                        error("Error: Valor de ambiente no reconocido '${Ambiente}'")
+                    }
                 }
-            }
             }
         }
     }
 }
-
